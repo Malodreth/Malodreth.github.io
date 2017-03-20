a9474d6e7376302562d0cdec134fb37f0871c677
Malodreth <maherrerahere2@gmail.com>
2017-03-20 01:57:33 -0700

    v0.16
    Altered favicon.ico, optimised blog.js and made minor changes to all
    html files.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,10 +6,10 @@ function addPosts() {
 	var bCred = document.querySelectorAll("#blog-author");
 	var counter = bRow.length;
 	for(var i=0;i<bRow.length;i++) {
-		bNum[i].insertAdjacentHTML("afterbegin", '<a href="./blog/post_'+counter+'.html">#'+counter+ '</a>');
+		bNum[i].innerHTML = '<a href="./blog/post_'+counter+'.html">#'+counter+ '</a>';
 		$(bTi[i]).load('./blog/post_'+counter+'.html #blog-header');
 		$(bDate[i]).load('./blog/post_'+counter+'.html #blog-footer');
-		bCred[i].insertAdjacentHTML("afterbegin", '<a href="./about.html">Malodreth</a>');
+		bCred[i].innerHTML = '<a href="./about.html">Malodreth</a>';
 		counter = counter - 1;
 	}
 }
@@ -17,14 +17,14 @@ function addPosts() {
 function pager() {
 	var pageNum = $("#post-no").text();
 	var realNum = parseInt(pageNum, 10);
-	var prev = document.querySelectorAll("#prev");
-	var next = document.querySelectorAll("#next");
+	var prev = document.querySelector("#prev");
+	var next = document.querySelector("#next");
 	var prevNum = realNum - 1;
 	var nextNum = realNum + 1;
 	if(prevNum != 0) {
-		prev[0].insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+prevNum+'.html">Prev</a>');
+		prev.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+prevNum+'.html">Prev</a>');
 	}else{
-		prev[0].insertAdjacentHTML("afterbegin", '<a href="../blog.html">Prev</a>');
+		prev.insertAdjacentHTML("afterbegin", '<a href="../blog.html">Prev</a>');
 	}
-	next[0].insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+nextNum+'.html">Next</a>');	
+	next.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+nextNum+'.html">Next</a>');	
 }
