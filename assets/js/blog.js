fd30a7b18b9602512c4cd6aa54c67572fe54d90d
Malodreth <maherrerahere2@gmail.com>
2017-03-20 20:56:44 -0700

    v0.18
    Revised js directory and added columns to blog post pages.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -6,25 +6,10 @@ function addPosts() {
 	var bCred = document.querySelectorAll("#blog-author");
 	var counter = bRow.length;
 	for(var i=0;i<bRow.length;i++) {
-		bNum[i].innerHTML = '<a href="./blog/post_'+counter+'.html">#'+counter+ '</a>';
+		bNum[i].innerHTML = '<a class="button btn-danger btn-xs" href="./blog/post_'+counter+'.html">'+counter+'</a>';
 		$(bTi[i]).load('./blog/post_'+counter+'.html #blog-header');
 		$(bDate[i]).load('./blog/post_'+counter+'.html #blog-footer');
 		bCred[i].innerHTML = '<a href="./about.html">Malodreth</a>';
 		counter = counter - 1;
 	}
-}
-
-function pager() {
-	var pageNum = $("#post-no").text();
-	var realNum = parseInt(pageNum, 10);
-	var prev = document.querySelector("#prev");
-	var next = document.querySelector("#next");
-	var prevNum = realNum - 1;
-	var nextNum = realNum + 1;
-	if(prevNum != 0) {
-		prev.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+prevNum+'.html">Prev</a>');
-	}else{
-		prev.insertAdjacentHTML("afterbegin", '<a href="../blog.html">Prev</a>');
-	}
-	next.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+nextNum+'.html">Next</a>');	
 }
---
+++ assets/js/post.js
@@ -0,0 +1,23 @@
+function pager() {
+	var pageNum = $("#post-no").text();
+	var realNum = parseInt(pageNum, 10);
+	var prev = document.querySelector("#prev");
+	var next = document.querySelector("#next");
+	var prevNum = realNum - 1;
+	var nextNum = realNum + 1;
+	if(prevNum != 0) {
+		prev.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+prevNum+'.html">Prev</a>');
+	}else{
+		prev.insertAdjacentHTML("afterbegin", '<a href="../blog.html">Prev</a>');
+	}
+	next.insertAdjacentHTML("afterbegin", '<a href="../blog/post_'+nextNum+'.html">Next</a>');	
+}
+
+var author=document.querySelector("#blog-footer");
+var avatar = document.querySelector("#blog-avatar");
+author.insertAdjacentHTML("afterbegin", '&raquo; Posted by <a href="../about.html">Malodreth</a> on ');
+avatar.innerHTML = '<img src="../assets/images/me4.jpg" width="150" height="150" alt="User image" /><br /><hr style="margin-bottom:5px;margin-top:10px;" /><p style="font-size:18px;"><b><a href="../about.html">Malodreth</a></b></p><p><b>Age:</b> 23</p><p><b>Location:</b> Bakersfield, CA</p><p><a class="btn btn-danger btn-xs" href="mailto:malodreth@gmail.com"><span class="glyphicon glyphicon-envelope"></span></a><a class="btn btn-danger btn-xs" href="http://www.facebook.com/malodreth" target="_blank"><span class="fa fa-facebook-official"></span></a></span></a><a class="btn btn-danger btn-xs" href="http://steamcommunity.com/id/malodreth" target="_blank"><span class="fa fa-steam-square"></span></a></p>';
+var avatarStyle = document.createElement("STYLE");
+var jsStylesheet = document.createTextNode("@media screen and (max-width:991px){#blog-avatar{display: none;}}@media screen and (min-width:1200px){#blog-avatar{min-height:500px;border-right:1px solid #EBCCD1;}#blog-content{min-height:400px;}#blog-avatar p{margin:2px;font-size:13px;}#post-no{padding-right:157px;border-right:1px solid #D9534F;}}");
+avatarStyle.appendChild(jsStylesheet);
+document.head.appendChild(avatarStyle);
