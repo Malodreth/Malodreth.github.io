dd86277f0e3e36eae45fdf7811e11d0a47cad1e9
Malodreth <malodreth@gmail.com>
2017-05-19 12:00:53 -0700

    v0.19
    Project veracruz 0.1.0-alpha

---
+++ Veracruz/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1495220319,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/touch_act-sheet0.png",
+		"images/player-sheet0.png",
+		"images/playerdirbox-sheet0.png",
+		"images/enemypizza-sheet0.png",
+		"images/enemygaycoconut-sheet0.png",
+		"images/weaponbaton-sheet0.png",
+		"images/weaponbattleaxe-sheet0.png",
+		"images/mainscreenkeyboard-sheet0.png",
+		"images/mainscreentouch-sheet0.png",
+		"images/grass.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
