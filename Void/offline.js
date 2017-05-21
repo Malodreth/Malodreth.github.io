711b68d3fb0d1023504feae5cd2afbafc2d85a73
Malodreth <malodreth@gmail.com>
2017-05-21 10:04:00 -0700

    v0.19
    Void test

---
+++ Void/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1495386183,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/toucha-sheet0.png",
+		"images/touchdown-sheet0.png",
+		"images/touchleft-sheet0.png",
+		"images/touchright-sheet0.png",
+		"images/touchup-sheet0.png",
+		"images/player-sheet0.png",
+		"images/collider-sheet0.png",
+		"images/leroy-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
